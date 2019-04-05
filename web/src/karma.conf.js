c4645cf695c807786c9bc4755b302de177ede258
Alex Rakoczy <scissorjammer@gmail.com>
2019-04-05 12:01:32 -0400

    TSLint karma.conf.js

--- web/src/karma.conf.js
+++ web/src/karma.conf.js
@@ -1,32 +1,32 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/web'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    basePath: "",
+    browsers: ["Chrome"],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    colors: true,
+    coverageIstanbulReporter: {
+      dir: require("path").join(__dirname, "../coverage/web"),
+      fixWebpackSourcePaths: true,
+      reports: ["html", "lcovonly", "text-summary"],
+    },
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    logLevel: config.LOG_INFO,
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    port: 9876,
+    reporters: ["progress", "kjhtml"],
+    restartOnFileChange: true,
     singleRun: false,
-    restartOnFileChange: true
   });
 };
