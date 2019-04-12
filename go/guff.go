3a4a922c39015aa0aed68a2c3fe01bc8c5b7568e
Alexander Rakoczy <arakoczy@gmail.com>
2019-04-11 22:37:15 -0400

    Graceful shutdown in guff.go

--- go/guff.go
+++ go/guff.go
@@ -1,32 +1,57 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"mime"
-	"net/http"
-	"path"
-	"path/filepath"
+  "context"
+  "flag"
+  "log"
+  "mime"
+  "net"
+  "net/http"
+  "os"
+  "os/signal"
+  "path"
+  "path/filepath"
+  "syscall"
+  "time"
 
-	"github.com/golang/glog"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+  "github.com/golang/glog"
+  "github.com/gorilla/handlers"
+  "github.com/gorilla/mux"
 )
 
 var (
 	webRoot = flag.String("web_root", "", "Path from which to serve web files.")
+  port = flag.String("port", "8080", "Port to listen for HTTP requests.")
 )
 
 func main() {
+  defer glog.Info("later, gator.")
 	flag.Parse()
 	glog.Info("Don't take any guff from these swine.")
 
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+  ctx = handleSigs(ctx)
+
 	r := mux.NewRouter()
 	r.PathPrefix("/").HandlerFunc(fileServerFunc(*webRoot))
-	err := http.ListenAndServe(":8080", handlers.CompressHandler(r))
-	if err != nil {
-		glog.Errorf("http.ListenAndServe() = %q", err)
-	}
+
+	s := &http.Server{Addr: net.JoinHostPort("", *port), Handler: handlers.CompressHandler(r)}
+
+	go func() {
+    glog.Infof("Listening on port %q", *port)
+    if err :=  s.ListenAndServe(); err != http.ErrServerClosed {
+      glog.Errorf("http.ListenAndServe() = %q", err)
+    }
+  }()
+  <-ctx.Done()
+
+  ctx, cancel = context.WithTimeout(context.Background(), 2 * time.Second)
+  defer cancel()
+
+  if err := s.Shutdown(ctx); err != nil {
+    glog.Errorf("Error shutting down http server: %q", err)
+  }
 }
 
 func fileServerFunc(root string) http.HandlerFunc {
@@ -42,4 +67,23 @@ func fileServerFunc(root string) http.HandlerFunc {
 		fs := http.FileServer(http.Dir(abs))
 		fs.ServeHTTP(w, r)
 	}
+}
+
+func handleSigs(ctx context.Context) context.Context {
+  sigint := make(chan os.Signal, 1)
+  signal.Notify(sigint, os.Interrupt)
+  signal.Notify(sigint, syscall.SIGTERM)
+
+  ctx, cancel := context.WithCancel(ctx)
+
+  go func() {
+    select {
+    case s := <-sigint:
+      glog.Infof("Handling signal %#v", s)
+      cancel()
+    case <-ctx.Done():
+    }
+  }()
+
+  return ctx
 }
