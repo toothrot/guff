fdba30972f54174542857a226bf121a3d95a275c
Alexander Rakoczy <arakoczy@gmail.com>
2019-05-21 17:05:48 -0400

    bootstrap Angular material #5
    ng add @angular/material
    
    Docs from https://material.angular.io/guide/getting-started

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
 ];
@@ -18,6 +19,7 @@ const routes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
